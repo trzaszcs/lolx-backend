c87d0e002609938680156bf017e7aac6fab7caf2
Slawomir T <s.trzaszczka@wp.pl>
2016-08-11 22:26:59 +0200

    offset added to random

--- src/main/groovy/pl/poznan/lolx/domain/AnounceSearchService.groovy
+++ src/main/groovy/pl/poznan/lolx/domain/AnounceSearchService.groovy
@@ -11,14 +11,14 @@ class AnounceSearchService {
         itemsPerPage = itemsPerPage < MAX_ITEMS_PER_PAGE ? itemsPerPage : MAX_ITEMS_PER_PAGE
         new SearchResult(
                 totalCount: 100,
-                anounces: generateAnounces(itemsPerPage)
+                anounces: generateAnounces(itemsPerPage, (page * itemsPerPage))
         )
     }
 
-    private def generateAnounces(count) {
+    private def generateAnounces(count, offset) {
         def anounces = []
         (0..count).each {
-            anounces.add(new Anounce(id: "${it}", title: "title ${it}", state: "wlkp", city: "Poz"))
+            anounces.add(new Anounce(id: "${offset + it}", title: "title ${offset + it}", state: "wlkp", city: "Poz"))
         }
         return anounces
     }
--- src/main/groovy/pl/poznan/lolx/rest/find/FindAnouncesEndpoint.groovy
+++ src/main/groovy/pl/poznan/lolx/rest/find/FindAnouncesEndpoint.groovy
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import pl.poznan.lolx.domain.AnounceSearchService
 import pl.poznan.lolx.domain.SearchResult
-import pl.poznan.lolx.rest.add.AnounceRequestDto
 
 @RestController
 @Slf4j
@@ -22,22 +21,21 @@ class FindAnouncesEndpoint {
     }
 
     @RequestMapping(value = "/anounces", method = RequestMethod.GET)
-    List<AnounceRequestDto> find(@RequestParam("query") String query,
-                                 @RequestParam(name = "page", defaultValue = "0") int page,
-                                 @RequestParam(name = "itemsPerPage", defaultValue = "20") int itemsPerPage) {
+    SearchResultDto find(@RequestParam("query") String query,
+                         @RequestParam(name = "page", defaultValue = "0") int page,
+                         @RequestParam(name = "itemsPerPage", defaultValue = "20") int itemsPerPage) {
         log.info("find anounces by query: {}", query)
         map(anounceSearchService.find(query, page, itemsPerPage))
     }
 
     def map(SearchResult searchResult) {
-        searchResult.collect {
-            new SearchResultDto(
-                    totalCount: it.totalCount,
-                    anounces: it.anounces.collect {
-                        new SimpleAnounceDto(id: it.id, title: it.title, state: it.state, city: it.city)
-                    }
-            )
-        }
+        new SearchResultDto(
+                totalCount: searchResult.totalCount,
+                anounces: searchResult.anounces.collect {
+                    new SimpleAnounceDto(id: it.id, title: it.title, state: it.state, city: it.city)
+                }
+        )
+
     }
 
 }
