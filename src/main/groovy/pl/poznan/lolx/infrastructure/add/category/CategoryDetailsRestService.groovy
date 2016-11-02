fe06cdef1bcf8f8e124cd840c6a803c24d3799d3
trzaszcs <s.trzaszczka@wp.pl>
2016-11-02 19:21:23 +0100

    configuration improved

--- src/main/groovy/pl/poznan/lolx/infrastructure/add/category/CategoryDetailsRestService.groovy
+++ src/main/groovy/pl/poznan/lolx/infrastructure/add/category/CategoryDetailsRestService.groovy
@@ -7,9 +7,6 @@ import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestTemplate
 import pl.poznan.lolx.domain.add.Category
 import pl.poznan.lolx.domain.add.CategoryDetails
-import pl.poznan.lolx.domain.add.User
-import pl.poznan.lolx.domain.add.UserDetails
-import pl.poznan.lolx.infrastructure.add.user.UserDto
 
 @Component
 class CategoryDetailsRestService implements CategoryDetails {
@@ -26,7 +23,7 @@ class CategoryDetailsRestService implements CategoryDetails {
             def response = restTemplate.getForEntity("${categoryAddress}/categories/${id}", CategoryDto)
             return Optional.of(new Category(id: id, name: response.body.name))
         } catch (HttpClientErrorException ex) {
-            if (ex.statusCode == 404)
+            if (ex.rawStatusCode == 404)
                 return Optional.empty()
             throw ex
         }
