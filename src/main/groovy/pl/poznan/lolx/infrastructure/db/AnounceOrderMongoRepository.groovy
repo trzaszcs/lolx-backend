342238e0b064af5da34df50150257076d65564d3
trzaszcs <s.trzaszczka@wp.pl>
2017-03-22 18:24:20 +0100

    old orders removed

--- src/main/groovy/pl/poznan/lolx/infrastructure/db/AnounceOrderMongoRepository.groovy
+++
@@ -1,8 +0,0 @@
-package pl.poznan.lolx.infrastructure.db
-
-import org.springframework.data.repository.CrudRepository
-
-interface AnounceOrderMongoRepository extends CrudRepository<AnounceOrderDocument, String> {
-    List<AnounceOrderDocument> getByCustomerId(String customerId)
-    List<AnounceOrderDocument> getByOwnerId(String ownerId)
-}
